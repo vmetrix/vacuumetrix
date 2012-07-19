4e36f6bf3b02146730f5967db0db97e5f339700a
David Lutz <david.lutz@99designs.com>
2012-07-20 00:42:54 +1000

    first stab at EBS

---
+++ bin/AWScloudwatchEBS.rb
@@ -0,0 +1,124 @@
+#!/usr/bin/env ruby
+## EBS stats.  We don't really use much EBS, so I don't know if this is going to be very useful.  Feedback please!
+### David Lutz
+### 2012-07-19
+### gem install fog  --no-ri --no-rdoc
+ 
+require 'rubygems'
+require 'fog'
+require '/opt/vacuumetrix/conf/config.rb'
+require '/opt/vacuumetrix/lib/Sendit.rb'
+
+#AWS cloudwatch stats for EBS seem to be at least 15 minutes behind and have a granualarity of 5 minutes
+
+startTime = Time.now.utc-3600
+endTime  = Time.now.utc-3300
+
+compute = Fog::Compute.new(:provider => :aws, :aws_secret_access_key => $awssecretkey, :aws_access_key_id => $awsaccesskey)
+instance_list = compute.volumes.all
+
+# ['VolumeWriteBytes', 'VolumeWriteOps', 'VolumeReadBytes', 'VolumeIdleTime', 'VolumeTotalReadTime', 'VolumeQueueLength', 'VolumeTotalWriteTime', 'VolumeReadOps']
+metricNames = ['VolumeWriteBytes', 'VolumeReadBytes']
+statistic = 'Sum'
+unit = 'Bytes'
+
+instance_list.each do |i|
+cloudwatch = Fog::AWS::CloudWatch.new(:aws_secret_access_key => $awssecretkey, :aws_access_key_id => $awsaccesskey)
+  
+  metricNames.each do |metricName|
+    response = cloudwatch.get_metric_statistics({
+           'Statistics' => statistic,
+           'StartTime' =>  startTime.iso8601,
+           'EndTime'    => endTime.iso8601,
+           'Period'     => 300,
+           'Unit'       => unit,
+           'MetricName' => metricName,
+           'Namespace'  => 'AWS/EBS',
+           'Dimensions' => [{
+                        'Name'  => 'VolumeId',
+                        'Value' => i.id 
+                        }]
+           }).body['GetMetricStatisticsResult']['Datapoints']
+
+  metricpath = "AWScloudwatch.EBS." + i.id + "." + metricName
+  begin
+        metricvalue = response.first[statistic]
+  rescue
+        metricvalue = 0
+  end
+  metrictimestamp = endTime.to_i.to_s
+
+  Sendit metricpath, metricvalue, metrictimestamp
+end
+end
+
+
+
+metricNames = ['VolumeWriteOps', 'VolumeQueueLength', 'VolumeReadOps']
+statistic = 'Sum'
+unit = 'Count'
+
+instance_list.each do |i|
+cloudwatch = Fog::AWS::CloudWatch.new(:aws_secret_access_key => $awssecretkey, :aws_access_key_id => $awsaccesskey)
+  
+  metricNames.each do |metricName|
+    response = cloudwatch.get_metric_statistics({
+           'Statistics' => statistic,
+           'StartTime' =>  startTime.iso8601,
+           'EndTime'    => endTime.iso8601,
+           'Period'     => 300,
+           'Unit'       => unit,
+           'MetricName' => metricName,
+           'Namespace'  => 'AWS/EBS',
+           'Dimensions' => [{
+                        'Name'  => 'VolumeId',
+                        'Value' => i.id 
+                        }]
+           }).body['GetMetricStatisticsResult']['Datapoints']
+
+  metricpath = "AWScloudwatch.EBS." + i.id + "." + metricName
+  begin
+        metricvalue = response.first[statistic]
+  rescue
+        metricvalue = 0
+  end
+  metrictimestamp = endTime.to_i.to_s
+
+  Sendit metricpath, metricvalue, metrictimestamp
+end
+end
+
+metricNames = ['VolumeIdleTime', 'VolumeTotalReadTime', 'VolumeTotalWriteTime', ]
+statistic = 'Average'
+unit = 'Seconds'
+
+instance_list.each do |i|
+cloudwatch = Fog::AWS::CloudWatch.new(:aws_secret_access_key => $awssecretkey, :aws_access_key_id => $awsaccesskey)
+  
+  metricNames.each do |metricName|
+    response = cloudwatch.get_metric_statistics({
+           'Statistics' => statistic,
+           'StartTime' =>  startTime.iso8601,
+           'EndTime'    => endTime.iso8601,
+           'Period'     => 300,
+           'Unit'       => unit,
+           'MetricName' => metricName,
+           'Namespace'  => 'AWS/EBS',
+           'Dimensions' => [{
+                        'Name'  => 'VolumeId',
+                        'Value' => i.id 
+                        }]
+           }).body['GetMetricStatisticsResult']['Datapoints']
+
+  metricpath = "AWScloudwatch.EBS." + i.id + "." + metricName
+  begin
+        metricvalue = response.first[statistic]
+  rescue
+        metricvalue = 0
+  end
+  metrictimestamp = endTime.to_i.to_s
+
+  Sendit metricpath, metricvalue, metrictimestamp
+end
+end
+
