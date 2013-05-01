94df1fb927d0be7114b9a9a04ca4189c34074b82
Brent Walker <brent@moveablecube.com>
2013-05-01 14:17:12 -0500

    accept input args process multiple responses use actual metric timestamp

--- bin/AWScloudwatchRDS.rb
+++ bin/AWScloudwatchRDS.rb
@@ -1,11 +1,11 @@
 #!/usr/bin/env ruby
-## grab metrics from AWS cloudwatch 
+## grab metrics from AWS cloudwatch
 ### David Lutz
 ### 2012-07-10
 ### gem install fog  --no-ri --no-rdoc
 ### if no argument is specified get metrics for all databases
 ### if a database name is specified as an argument then only show that database
- 
+
 $:.unshift File.join(File.dirname(__FILE__), *%w[.. conf])
 $:.unshift File.join(File.dirname(__FILE__), *%w[.. lib])
 
@@ -14,91 +14,87 @@ require 'Sendit'
 require 'rubygems'
 require 'fog'
 require 'json'
+require 'optparse'
 
-if ARGV.length == 1
-  dimensionId = ARGV[0]
+options = {
+    :start_offset => 180,
+    :end_offset => 120
+}
+
+optparse = OptionParser.new do |opts|
+  opts.banner = "Usage: AWScloudwatchRDS.rb [options] [DBInstanceIdentifier]"
+
+  opts.on('-s', '--start-offset [OFFSET_SECONDS]', 'Time in seconds to offset from current time as the start of the metrics period. Default 1200') do |s|
+    options[:start_offset] = s
+  end
+
+  opts.on('-e', '--end-offset [OFFSET_SECONDS]', 'Time in seconds to offset from current time as the start of the metrics period. Default 600') do |e|
+    options[:end_offset] = e
+  end
+
+  opts.on('-h', '--help', '') do
+    puts opts
+    exit
+  end
 end
 
-#AWS cloudwatch stats seem to be a minute or so behind
+optparse.parse!
 
-startTime = Time.now.utc-180
-endTime  = Time.now.utc-120
 
-metricNames = {	"CPUUtilization" 	=> "Percent", 
-		"DatabaseConnections" 	=> "Count",
-		"FreeStorageSpace" 	=> "Bytes",
-		"ReadIOPS"		=> "Count/Second",
- 		"ReadLatency"		=> "Seconds",
-		"ReadThroughput"	=> "Bytes/Second",
-		"WriteIOPS"		=> "Count/Second",
- 		"WriteLatency"		=> "Seconds",
-		"WriteThroughput"	=> "Bytes/Second",
-		"ReplicaLag"		=> "Seconds",
-	}
-
-statisticTypes = 'Average'
-
-rds = Fog::AWS::RDS.new(:aws_secret_access_key => $awssecretkey, :aws_access_key_id => $awsaccesskey)
-
-if !dimensionId.nil?
-  instance_list = rds.servers.get(dimensionId)
-  cloudwatch = Fog::AWS::CloudWatch.new(:aws_secret_access_key => $awssecretkey, :aws_access_key_id => $awsaccesskey)
-
-  metricNames.each do |metricName, unit|
-    begin
-      response = cloudwatch.get_metric_statistics({
-           'Statistics' => 'Average',
-           'StartTime' =>  startTime.iso8601,
-           'EndTime'    => endTime.iso8601, 
-	   'Period'     => 60, 
-           'Unit'       => unit,
-	   'MetricName' => metricName, 
-	   'Namespace'  => 'AWS/RDS',
-	   'Dimensions' => [{
-	                'Name'  => 'DBInstanceIdentifier', 
-	                'Value' => dimensionId 
-			}]
-           }).body['GetMetricStatisticsResult']['Datapoints']
-
-      metricpath = "AWScloudwatch.RDS." + dimensionId + "." + metricName 
-      metricvalue = response.first["Average"]
-      metrictimestamp = endTime.to_i.to_s
-
-      Sendit metricpath, metricvalue, metrictimestamp
-    rescue
-# silently catch error when there's no ReplicaLag metric
-    end
+dbNames = []
+if ARGV.length > 0
+  ARGV.each do |db|
+    dbNames << db
   end
-  else
-    instance_list = rds.servers.all
+else
+  rds = Fog::AWS::RDS.new(:aws_secret_access_key => $awssecretkey, :aws_access_key_id => $awsaccesskey)
+  rds.servers.all.each do |s|
+    dbNames << s.id
+  end
+end
 
-    instance_list.each do |i|
-    cloudwatch = Fog::AWS::CloudWatch.new(:aws_secret_access_key => $awssecretkey, :aws_access_key_id => $awsaccesskey)
+startTime = Time.now.utc - options[:start_offset].to_i
+endTime = Time.now.utc - options[:end_offset].to_i
 
-    dimensionId = i.id
-    metricNames.each do |metricName, unit|
+metricNames = {"CPUUtilization" => "Percent",
+               "DatabaseConnections" => "Count",
+               "FreeStorageSpace" => "Bytes",
+               "ReadIOPS" => "Count/Second",
+               "ReadLatency" => "Seconds",
+               "ReadThroughput" => "Bytes/Second",
+               "WriteIOPS" => "Count/Second",
+               "WriteLatency" => "Seconds",
+               "WriteThroughput" => "Bytes/Second",
+               "ReplicaLag" => "Seconds",
+}
+
+
+cloudwatch = Fog::AWS::CloudWatch.new(:aws_secret_access_key => $awssecretkey, :aws_access_key_id => $awsaccesskey)
+
+dbNames.each do |db|
+  metricNames.each do |metricName, unit|
+    responses = cloudwatch.get_metric_statistics({
+                                                     'Statistics' => 'Average',
+                                                     'StartTime' => startTime.iso8601,
+                                                     'EndTime' => endTime.iso8601,
+                                                     'Period' => 60,
+                                                     'Unit' => unit,
+                                                     'MetricName' => metricName,
+                                                     'Namespace' => 'AWS/RDS',
+                                                     'Dimensions' => [{
+                                                                          'Name' => 'DBInstanceIdentifier',
+                                                                          'Value' => db
+                                                                      }]
+                                                 }).body['GetMetricStatisticsResult']['Datapoints']
+
+    responses.each do |response|
+      metricpath = "AWScloudwatch.RDS." + db + "." + metricName
       begin
-        response = cloudwatch.get_metric_statistics({
-           'Statistics' => 'Average',
-           'StartTime' =>  startTime.iso8601,
-           'EndTime'    => endTime.iso8601, 
-	   'Period'     => 60, 
-           'Unit'       => unit,
-	   'MetricName' => metricName, 
-	   'Namespace'  => 'AWS/RDS',
-	   'Dimensions' => [{
-	                'Name'  => 'DBInstanceIdentifier', 
-	                'Value' => dimensionId 
-			}]
-           }).body['GetMetricStatisticsResult']['Datapoints']
-
-        metricpath = "AWScloudwatch.RDS." + dimensionId + "." + metricName 
-        metricvalue = response.first["Average"]
-        metrictimestamp = endTime.to_i.to_s
-
+        metricvalue = response["Average"]
+        metrictimestamp = response["Timestamp"].to_i.to_s
         Sendit metricpath, metricvalue, metrictimestamp
       rescue
-# silently catch error when there's no ReplicaLag metric
+        # ignored
       end
     end
   end
