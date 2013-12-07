de45faf9717c878a1d12e252599c33f79f712426
Drew Rothstein <drothstein@twitter.com>
2013-12-07 18:10:44 -0500

    update twitter.rb to use v1.1 api as v1 is deprecated, add to config.rb-sample, uniform example spacing

--- bin/twitter.rb
+++ bin/twitter.rb
@@ -2,6 +2,9 @@
 ### How many twitter followers do we have?
 ### David Lutz
 ### 2012-07-23
+###
+### Moved to api v1.1 as v1 is deprecated
+### 2013-12-07
 
 ## new versions of ruby don't need the following line
 $:.unshift File.join(File.dirname(__FILE__), *%w[.. conf])
@@ -10,26 +13,28 @@ $:.unshift File.join(File.dirname(__FILE__), *%w[.. lib])
 require 'config'
 require 'Sendit'
 require 'rubygems'
-require 'curb'
+require 'twitter'
 require 'json'
 
 if ARGV.length != 1
-  puts "I need one arguments. The name of the twit."
+  puts "I need one argument, the name of the twitter user."
   exit 1
+end
+
+client = Twitter::REST::Client.new do |config|
+  config.consumer_key        = $tw_consumer_key
+  config.consumer_secret     = $tw_consumer_secret
+  config.access_token        = $tw_access_token
+  config.access_token_secret = $tw_access_token_secret
 end
 
 user = ARGV[0]
 
-metricURL = "https://api.twitter.com/1/users/show.json?screen_name=" + user
-
-response = Curl::Easy.perform(metricURL) do |curl| 
-end
-
-body = response.body_str
-result = JSON.parse(body)
+user_object		= client.user(user)
+followers_count	= user_object.followers_count
 
 metricpath = "twitter.followers." + user
-metricvalue = result["followers_count"]
+metricvalue = followers_count
 metrictimestamp = Time.now.utc.to_i.to_s
 
 Sendit metricpath, metricvalue, metrictimestamp
