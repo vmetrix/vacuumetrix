9682f8907b5105efe91a8c89dfd8c889c01b1249
Ian Bissett <ian.bissett@bigcommerce.com>
2014-02-11 10:09:54 +1100

    Raise errors on New Relic responses

--- bin/NewrelicThresholds.rb
+++ bin/NewrelicThresholds.rb
@@ -29,6 +29,8 @@ def GetThresholdMetrics(application, appname)
 
 	body=response.body_str
 	data = XmlSimple.xml_in(body, { 'KeyAttr' => 'threshold_values' })
+	raise 'No data returned from New Relic API' if data['content'] === "\n"
+	raise "Error returned from New Relic API: #{data['content']}" if data['content']
 	data['threshold_value'].each do |item|
 	Sendit "newrelic." + appname.gsub( /[ \.]/, "_") + "." + item['name'].gsub(" ","_"), item['metric_value'], $timenow.to_s
 	end
